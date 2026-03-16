dfc047f6382e504765e34e87b239cc1abd20f4ae
Artem Tarakanov <artemtarakanov2505@gmail.com>
2026-03-16 21:27:51 +0300

    Добавлена секция Hero

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import './App.css'
 import Header from './components/Header';
+import Hero from "./components/Hero.tsx";
 
 function App() {
   return (
     <>
     <section id='#home'>
       <Header/>
+    </section>
+
+    <section id='#home'>
+      <Hero/>
     </section>
     </>
   )
