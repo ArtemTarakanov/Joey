6722ceab94338fc2babee1f5405e0363ab345f36
Artem Tarakanov <artemtarakanov2505@gmail.com>
2026-03-18 23:31:58 +0300

    Реализована горизонтальная прокрутка

--- src/components/Expertise.tsx
+++ src/components/Expertise.tsx
@@ -24,18 +24,19 @@ export default function Expertise(){
     const {isDark} = useTheme();
     return(
         <section className={`h-auto flex items-center justify-center px-28 max-md:px-4 py-19 max-md:py-12 ${isDark ? 'bg-black' : 'bg-white'}` }>
-            <div className={`flex flex-col gap-11 max-md:gap-3.5 `}>
+            <div className={`flex flex-col gap-11 max-md:gap-3.5 w-full`}>
                 <h2 className={`font-['Work Sans'] font-semibold text-5xl max-md:text-2xl leading-[120%] tracking-[-1%] ${isDark?`text-white`:`text-black`}`}>Expertise</h2>
 
-                <div className = {`grid grid-cols-2 max-md:grid-cols-1 gap-x-68.5 gap-y-20 max-md:gap-y-8 justify-center w-full`}>
-                    {cards.map((card, index) => (
-                        <div key={index}>
-                            <h3 className={`font-['Work Sans'] font-semibold text-3xl max-md:text-xl leading-[130%] tracking-[-1%] mb-4 max-md:mb-3 ${isDark?`text-white before:content-['•'] before:mr-2 before:text-white`:`text-black before:content-['•'] before:mr-2 before:text-black`}`}>{card.title}</h3>
-                            <p className={`font-['Work Sans'] font-medium text-xl max-md:text-sm leading-[140%] tracking-[-1%] max-w-112 ${isDark?`text-white`:`text-black`}`}>{card.description}</p>
-                        </div>
-                    ))}
+                <div className="overflow-x-auto max-md:w-full" style={{scrollbarWidth: 'none', msOverflowStyle: 'none'}}>
+                    <div className = {`grid max-md:flex grid-cols-2 gap-x-68.5 gap-y-20 max-md:gap-x-6 max-md:gap-y-8 justify-center w-full max-md:w-max`}>
+                        {cards.map((card, index) => (
+                            <div key={index} className="max-md:flex-shrink-0 max-md:w-64">
+                                <h3 className={`font-['Work Sans'] font-semibold text-3xl max-md:text-xl leading-[130%] tracking-[-1%] mb-4 max-md:mb-3 ${isDark?`text-white before:content-['•'] before:mr-2 before:text-white`:`text-black before:content-['•'] before:mr-2 before:text-black`}`}>{card.title}</h3>
+                                <p className={`font-['Work Sans'] font-medium text-xl max-md:text-sm leading-[140%] tracking-[-1%] max-w-112 ${isDark?`text-white`:`text-black`}`}>{card.description}</p>
+                            </div>
+                        ))}
+                    </div>
                 </div>
-
 
             </div>
         </section>
