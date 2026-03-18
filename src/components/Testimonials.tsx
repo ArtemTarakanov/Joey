1c166bc2455d188c0509d8bb1dac93900403dc32
Artem Tarakanov <artemtarakanov2505@gmail.com>
2026-03-18 22:08:34 +0300

    Реализована секция Testimonials

---
+++ src/components/Testimonials.tsx
@@ -0,0 +1,21 @@
+import {useTheme} from "../context/ThemeContext.tsx";
+
+export default function Testimonials(){
+    const {isDark} = useTheme();
+    return(
+        <section className={`h-full flex items-center justify-center py-20 max-md:py-12 px-28 max-md:px-4 ${isDark?`bg-black`:`bg-white`}`}>
+            <div className={`flex flex-col gap-4 max-md:gap-6`}>
+                <h2 className={`font-['Work Sans'] font-semibold text-[64px] max-md:text-2xl max-md:leading-[130%]  leading-[110.00000000000001%] tracking-[-1%] ${isDark?`text-white`:`text-black`}`}>Testimonials</h2>
+
+                <div className={`flex flex-row max-md:flex-col gap-20 max-md:gap-4 items-start`}>
+                <p className={`font-['Work Sans'] font-medium text-5xl max-md:text-sm leading-[130%] max-md:leading-6 tracking-[-1%] max-w-238 ${isDark?`text-white`:`text-black`}`}>“Joey is a remarkable artist. He grasps abstract ideas and transforms them into exceptional visuals. Throughout the years, I've witnessed him produce everything from small symbols to comprehensive adaptive web pages. He has crafted clothing items like tees and caps, promotional products, and professional literature.”</p>
+
+                    <span className={`flex flex-col gap-2.125`}>
+                        <h3 className={`font-['Work Sans'] font-bold text-xl max-md:text-sm leading-[140%] tracking-[-1%] ${isDark?`text-white`:`text-black`}`}>Chris Ellington</h3>
+                        <p className={`font-['Work Sans'] font-medium text-lg max-md:text-xs max-md:leading-6 leading-[140%] tracking-[-1%] ${isDark?`text-white`:`text-black`}`}>Product Lead,Gatha.</p>
+                    </span>
+                </div>
+            </div>
+        </section>
+    );
+}
