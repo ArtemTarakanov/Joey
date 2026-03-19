ed3df41beef71d94bf7ad2aae6f992cf7ba9c535
Artem Tarakanov <artemtarakanov2505@gmail.com>
2026-03-19 15:54:25 +0300

    Реализован плавный скролл к якорным секциям

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import { useTheme } from '../context/ThemeContext';
 import { useActiveAnchor } from '../hooks/useActiveAnchor';
+import {Link} from 'react-scroll';
 
 export default function Header(){
     const [isOpen, setIsOpen] = useState(false);
@@ -24,48 +25,69 @@ export default function Header(){
 
 
                     <ul className={`rounded-[80px] flex gap-8 h-14 items-center py-2.25 px-3 fixed top-8 left-1/2 -translate-x-1/2 max-md:hidden ${isDark ? 'bg-white' : 'bg-black'}`}>
-                        <a
-                            href="#home"
-                            className={`font-['Work Sans'] font-medium text-xl leading-4 -tracking-[1%] px-4 py-3 rounded-full transition-all ${
+                        <Link
+                            to = "home"
+                            smooth = {true}
+                            duration = {500}
+                            offset = {-80}
+                            spy = {true}
+                            activeClass = "active"
+                            className={`cursor-pointer font-['Work Sans'] font-medium text-xl leading-4 -tracking-[1%] px-4 py-3 rounded-full transition-all ${
                                 isActive('home')
                                     ? isDark ? 'bg-black text-white' : 'bg-white text-black'
                                     : isDark ? 'text-black' : 'text-[#C4C4C4]'
                             }`}
                         >
                             Home
-                        </a>
-                        <a
-                            href="#projects"
-                            className={`font-['Work Sans'] font-medium text-xl leading-4 -tracking-[1%] px-4 py-3 rounded-full transition-all ${
+                        </Link>
+
+                        <Link
+                            to = "projects"
+                            duration = {500}
+                            offset = {-80}
+                            smooth = {true}
+                            spy = {true}
+                            activeClass = "active"
+                            className={` cursor-pointer font-['Work Sans'] font-medium text-xl leading-4 -tracking-[1%] px-4 py-3 rounded-full transition-all ${
                                 isActive('projects')
                                     ? isDark ? 'bg-black text-white' : 'bg-white text-black'
                                     : isDark ? 'text-black' : 'text-[#C4C4C4]'
                             }`}
                         >
                             Projects
-                        </a>
-                        <a
-                            href="#about"
-                            className={`font-['Work Sans'] font-medium text-xl leading-4 -tracking-[1%] px-4 py-3 rounded-full transition-all ${
+                        </Link>
+                        <Link
+                            to = "about"
+                            smooth = {true}
+                            duration = {500}
+                            spy = {true}
+                            activeClass = "active"
+                            offset = {-80}
+                            className={`cursor-pointer font-['Work Sans'] font-medium text-xl leading-4 -tracking-[1%] px-4 py-3 rounded-full transition-all ${
                                 isActive('about')
                                     ? isDark ? 'bg-black text-white' : 'bg-white text-black'
                                     : isDark ? 'text-black' : 'text-[#C4C4C4]'
                             }`}
                         >
                             About
-                        </a>
-                        <a
-                            href="#contact"
-                            className={`font-['Work Sans'] font-medium text-xl leading-4 -tracking-[1%] px-4 py-3 rounded-full transition-all ${
+                        </Link>
+                        <Link
+                            to= "contact"
+                            smooth = {true}
+                            duration = {500}
+                            spy = {true}
+                            activeClass = "active"
+                            offset = {-80}
+                            className={`cursor-pointer font-['Work Sans'] font-medium text-xl leading-4 -tracking-[1%] px-4 py-3 rounded-full transition-all ${
                                 isActive('contact')
                                     ? isDark ? 'bg-black text-white' : 'bg-white text-black'
                                     : isDark ? 'text-black' : 'text-[#C4C4C4]'
                             }`}
                         >
                             Contact
-                        </a>
+                        </Link>
 
-                        <button onClick={toggleTheme} className={`w-10 h-10 rounded-[50%] flex items-center justify-center ${isDark ? 'bg-black' : 'bg-white'}`}>
+                        <button onClick={toggleTheme} className={`cursor-pointer w-10 h-10 rounded-[50%] flex items-center justify-center ${isDark ? 'bg-black' : 'bg-white'}`}>
                             {isDark ? (
                                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="white" viewBox="0 0 16 16">
                                     <path d="M12 8a4 4 0 1 1-8 0 4 4 0 0 1 8 0M8 0a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 0m0 13a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 13m8-5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2a.5.5 0 0 1 .5.5M3 8a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2A.5.5 0 0 1 3 8m10.657-5.657a.5.5 0 0 1 0 .707l-1.414 1.415a.5.5 0 1 1-.707-.708l1.414-1.414a.5.5 0 0 1 .707 0m-9.193 9.193a.5.5 0 0 1 0 .707L3.05 13.657a.5.5 0 0 1-.707-.707l1.414-1.414a.5.5 0 0 1 .707 0m9.193 2.121a.5.5 0 0 1-.707 0l-1.414-1.414a.5.5 0 0 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .707M4.464 4.465a.5.5 0 0 1-.707 0L2.343 3.05a.5.5 0 1 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .708"/>
